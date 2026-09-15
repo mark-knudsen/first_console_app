ebda1482aec41a54198c20dc0b2d664efc0c5616
mark-knudsen <marksvendstrupknudsen@gmail.com>
2026-09-15 13:30:23 +0200

    added first commit for first_branch

--- my_first_console_app/Program.cs
+++ my_first_console_app/Program.cs
@@ -1,4 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
 
+
+int number = 100;
+float myFloat = 20.00;
+
+Console.WriteLine($"number: {number}, myFloat: {myFloat}");
+Console.ReadLine();
 // Comments
 Console.ReadLine();
